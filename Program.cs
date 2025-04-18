b89d39965c21ccd347769bf213c4d73869fc27a5
HS-9006 <nsin99153@gmail.com>
2025-04-18 14:00:37 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -52,35 +52,35 @@ class GameLogic
         {
             Console.WriteLine("잘못된 이름입니다.");
             Thread.Sleep(1000);
-            Init(); 
+            Init();
         }
         else
         {
             _player = new Player(playerName);
             Console.WriteLine($"{_player.name}님, 입장하셨습니다.");
         }
-        
-            // 직업선택
-            Console.WriteLine("직업을 선택하세요. [1:전사 | 2:법사 | 3:궁수]");
-            int job = int.Parse(Console.ReadLine());
 
-            if (job >= 1 && job <= 3)  
+        // 직업선택
+        Console.WriteLine("직업을 선택하세요. [1:전사 | 2:법사 | 3:궁수]");
+        int job = int.Parse(Console.ReadLine());
+
+        if (job >= 1 && job <= 3)
+        {
+            _player.job = (Job)job;
+
+            switch (_player.job)
             {
-                _player.job = (Job)job;
-
-                switch (_player.job)
-                {
-                    case Job.Warrior:
-                        Console.WriteLine($"{_player.job}를 선택했습니다.");
-                        break;
-                    case Job.Wizzard:
-                        Console.WriteLine($"{_player.job}를 선택했습니다.");
-                        break;
-                    case Job.Archor:
-                        Console.WriteLine($"{_player.job}를 선택했습니다.");
-                        break;
-                }
+                case Job.Warrior:
+                    Console.WriteLine($"{_player.job}를 선택했습니다.");
+                    break;
+                case Job.Wizzard:
+                    Console.WriteLine($"{_player.job}를 선택했습니다.");
+                    break;
+                case Job.Archor:
+                    Console.WriteLine($"{_player.job}를 선택했습니다.");
+                    break;
             }
+        }
         bool isRunning = true;
 
         while (isRunning)
@@ -89,31 +89,42 @@ class GameLogic
             Console.WriteLine("1. 상태보기\n2. 인벤토리\n3. 상점");
             string information = Console.ReadLine();
 
-            
-                var ironArmor = new ItemData("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 5, ItemType.armor, 500);
-                var spartanSpear = new ItemData("스파르타의 창", "스파르타 전사들이 사용했다는 전설의 창입니다.", 7, 0, ItemType.spear, 800);
 
-                if (information == "1")
+            var ironArmor = new ItemData("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 5, ItemType.armor, 500);
+            var spartanSpear = new ItemData("스파르타의 창", "스파르타 전사들이 사용했다는 전설의 창입니다.", 7, 0, ItemType.spear, 800);
+
+            if (information == "1")
+            {
+                bool isInfo = true;
+                while (isInfo)
                 {
+
                     Console.WriteLine("상태보기 입니다.");
                     Console.WriteLine("캐릭터의 정보가 표시됩니다.");
                     Console.WriteLine($"Lv : {_player.level}\n" + $"{_player.name}" + "(" + $"{_player.job}" + ")\n" +
                         $"공격력 : {_player.attack}\n" + $"방어력 : {_player.defense}\n" + $"체력 : {_player.stamina}\n" + $"Gold : {_player.money}\n");
                     Console.WriteLine("0. 나가기");
-                 }
+                    int infoOut = int.Parse(Console.ReadLine());
+
+                    if (infoOut == 0)
+                    {
+                        isInfo = false;
+                    }
+                }
+            }
 
             else if (information == "2")
             {
                 bool isWearing = true;
                 while (isWearing)
                 {
-                    
+
                     Console.WriteLine("인벤토리 입니다.");
                     Console.WriteLine("1. 장착 관리");
                     Console.WriteLine("0. 나가기");
                     int wearing = int.Parse(Console.ReadLine());
 
-                    if(wearing == 0)
+                    if (wearing == 0)
                     {
                         isWearing = false;
                     }
@@ -124,11 +135,11 @@ class GameLogic
                         {
                             Console.WriteLine("보유 중인 아이템이 없습니다.");
                         }
-                        else 
+                        else
                         {
                             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
                             Console.WriteLine("[아이템 목록]");
-                            for(int i = 0; i < _player.inventory.Count; i++)
+                            for (int i = 0; i < _player.inventory.Count; i++)
                             {
                                 var item = _player.inventory[i];
                                 bool isEquipped = (item == _player.equippedWeapon || item == _player.equippedArmor);
@@ -155,16 +166,16 @@ class GameLogic
                 };
                 while (isShopping)
                 {
-                Console.WriteLine("상점 입니다.");
-                Console.WriteLine("[보유골드]\n"+ $"{_player.money}G\n");
-                Console.WriteLine("아이템목록");
+                    Console.WriteLine("상점 입니다.");
+                    Console.WriteLine("[보유골드]\n" + $"{_player.money}G\n");
+                    Console.WriteLine("아이템목록");
                     for (int i = 0; i < shopItems.Count; i++)
                     {
                         var item = shopItems[i];
                         bool already = _player.inventory.Exists(x => x.name == item.name);
                         string priceLabel = already ? "구매완료" : $"{item.money}G";
 
-                        Console.WriteLine($"{i +1}.{item.name} | 공격력 + {item.attack} | 방어력 +{item.defense} | {item.description} | {priceLabel}");
+                        Console.WriteLine($"{i + 1}.{item.name} | 공격력 + {item.attack} | 방어력 +{item.defense} | {item.description} | {priceLabel}");
                     }
                     Console.WriteLine("\n1. 아이템 구매");
                     Console.WriteLine("0. 나가기");
@@ -175,15 +186,15 @@ class GameLogic
                     {
                         isShopping = false;
                     }
-                    else if(shopInput == "1")
+                    else if (shopInput == "1")
                     {
                         Console.WriteLine("구매할 아이템 번호를 입력하세요");
                         string storeNum = Console.ReadLine();
                         int buyIndex;
-                        if(int.TryParse(storeNum, out buyIndex))
+                        if (int.TryParse(storeNum, out buyIndex))
                         {
                             buyIndex -= 1;
-                            if(buyIndex >= 0 && buyIndex < shopItems.Count)
+                            if (buyIndex >= 0 && buyIndex < shopItems.Count)
                             {
                                 var itemBuy = shopItems[buyIndex];
 
@@ -233,7 +244,7 @@ class Player
     public int money;
     public ItemType type;
 
-    public List<ItemData> inventory = new List<ItemData>(); 
+    public List<ItemData> inventory = new List<ItemData>();
     public ItemData equippedWeapon = null;
     public ItemData equippedArmor = null;
 
